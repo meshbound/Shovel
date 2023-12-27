059adaa62347e43eb02f6aa20081c41d66c20475
Owen Z <16374972+Apollogeist@users.noreply.github.com>
2023-12-26 21:54:02 -0600

    Add Windows placeholder TTS

--- src/lib/generate_speech.py
+++ src/lib/generate_speech.py
@@ -1,6 +1,7 @@
 import os
 import time
 import math
+import pyttsx3
 from google.cloud import texttospeech
 from moviepy.editor import AudioFileClip
 
@@ -17,7 +18,8 @@ class SpeechGenerator:
         print(f"Generating speech from text: {text}")
 
         if self.use_placeholder:
-            return AudioFileClip('src/lib/test_audio.wav')
+            audio_path = self.generate_placeholder_speech(text)
+            return AudioFileClip(audio_path)
         
         input_text = texttospeech.SynthesisInput(text=text)
         voice = texttospeech.VoiceSelectionParams(
@@ -34,11 +36,37 @@ class SpeechGenerator:
             audio_config=audio_config
         )
 
+        audio = self.write_to_timestamped_file(response.audio_content)
+        return audio
+    
+    @staticmethod
+    def generate_placeholder_speech(text) -> str:
+        # Initialize the TTS engine
+        engine = pyttsx3.init()
+
+        # Set properties (optional)
+        engine.setProperty('rate', 150)  # Speed of speech
+        engine.setProperty('volume', 0.9)  # Volume level (0.0 to 1.0)
+
+        # Save to audio file (WAV format)
+        filename = math.floor(time.time() * 1000)
+        file_path = f"src/lib/audio_tmp/{filename}.wav"
+        engine.save_to_file(text, file_path)
+
+        # Wait for the file to be created
+        while not engine.isBusy():
+            time.sleep(0.1)
+
+        # Run the TTS engine
+        engine.runAndWait()
+
+        return file_path
+
+    @staticmethod
+    def write_to_timestamped_file(data) -> AudioFileClip:
         filename = math.floor(time.time() * 1000)
         file_path = f"src/lib/audio_tmp/{filename}.mp3"
         with open(file_path, "wb") as out:
-            out.write(response.audio_content)
+            out.write(data)
 
-        audio = AudioFileClip(file_path)
-
-        return audio
+        return AudioFileClip(file_path)
